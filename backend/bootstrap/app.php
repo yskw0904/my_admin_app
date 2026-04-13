91a7d66d09096be5c7a1e7482240cfde6847922a
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-13 10:02:09 +0900

    API初期設定

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,18 +1,31 @@
-<?php
-
+<?
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // APIのルーティング
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // 未認証時のリダイレクト先をカスタマイズ（APIの時はnullを返してエラーにする）
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return null;
+            }
+            return route('login');
+        });
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        // どんなエラーが発生しても、URLが api/ から始まっていれば強制的にJSONで返す
+        $exceptions->shouldRenderJsonWhen(function (Request $request, \Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+            return $request->expectsJson();
+        });
     })->create();
---
+++ backend/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/hello', function () {
+    return [
+        'message' => 'hello world!',
+        'status' => 200
+    ];
+});
