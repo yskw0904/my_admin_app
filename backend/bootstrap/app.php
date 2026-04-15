91995a79b0e9c68f8670c8be0737c23f9e6f1864
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-15 09:50:23 +0900

    エラー「session store not set on request.」対策

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Session\Middleware\StartSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // SanctumのSPA用ミドルウェアを有効化
         $middleware->statefulApi();
+        // StartSessionミドルウェアを有効
+        $middleware->append(StartSession::class);
 
         // 未認証時のリダイレクト先をカスタマイズ（APIの時はnullを返してエラーにする）
         $middleware->redirectGuestsTo(function (Request $request) {
