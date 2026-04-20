29f869b5c26d9de65af0cc491717815bd4f625a5
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-20 11:08:35 +0900

    アクティビティのバリデーション作成

---
+++ backend/app/Http/Requests/StoreActivityRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\ActivityType;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class StoreActivityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'activity_type' => ['required', new Enum(ActivityType::class)],
+            'start_time'    => ['required', 'date'],
+            'end_time'      => ['required', 'date', 'after:start_time'], // 開始より後であること
+            'note'          => ['nullable', 'string', 'max:1000'],
+        ];
+    }
+}
