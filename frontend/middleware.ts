db5b4182894697d0ecd188492f99a65dc217687e
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-22 00:10:57 +0900

    未ログイン時のリダイレクト設定追加

---
+++ frontend/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const session = request.cookies.get('laravel_session');
+
+    if (!session && request.nextUrl.pathname !== '/login') {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (session && request.nextUrl.pathname.startsWith('/login')) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+// ミドルウェアを起動させるURL（ルート）を指定
+export const config = {
+    matcher: [
+    /*
+     * 適用したいパスを指定します。
+     * '/' (TOP画面のみ) や '/dashboard/:path*' (ダッシュボード配下すべて) など
+     * APIルートや静的ファイル(_next/static等)は除外するのが一般的です。
+     */
+    '/',
+    '/activity/:path*',
+    '/login',
+    ],
+}
