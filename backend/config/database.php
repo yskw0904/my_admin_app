be730cf9fcd80ad09d692d0ca66f75ff8d5bf001
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-21 09:35:41 +0900

    データベースのタイムゾーン修正

--- backend/config/database.php
+++ backend/config/database.php
@@ -62,6 +62,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'timezone' => '+09:00',
         ],
 
         'mariadb' => [
