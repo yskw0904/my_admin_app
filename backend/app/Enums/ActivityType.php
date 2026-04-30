10f2c58153425097e6357c3092c76059ad51eebc
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-30 18:15:06 +0900

    アクティビティ種別にラベル追加

--- backend/app/Enums/ActivityType.php
+++ backend/app/Enums/ActivityType.php
@@ -10,4 +10,17 @@ enum ActivityType: string
     case NAP = 'nap';
     case WORK = 'work';
     case PHONE = 'phone';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::SLEEP => '睡眠',
+            self::MEAL => '食事',
+            self::WALK => '散歩',
+            self::READ => '読書',
+            self::NAP => '昼寝',
+            self::WORK => '作業',
+            self::PHONE => 'スマホ',
+        };
+    }
 }
