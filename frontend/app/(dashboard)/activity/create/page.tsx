5f74ba377be46431c5d42b57560e05bef9e44228
yskw0904 <yoshikawa.y0904@gmail.com>
2026-04-30 18:16:19 +0900

    生活リズムの記録画面にダイアログ追加・種別の選択欄追加

--- frontend/app/(dashboard)/activity/create/page.tsx
+++ frontend/app/(dashboard)/activity/create/page.tsx
@@ -1,7 +1,54 @@
-export default function Create() {
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+async function getActivityTypes() {
+  const res = await fetch(`${process.env.API_URL}/constants/activity-types`, {
+    cache: "no-store",
+  });
+  if (!res.ok) throw new Error("取得失敗");
+  const json = await res.json();
+  return json.data;
+}
+
+type ActivityType = {
+  id: string;
+  name: string;
+};
+
+export default async function Create() {
+  const types: ActivityType[] = await getActivityTypes();
   return (
     <>
       <h1>生活リズムの記録</h1>
+      <Dialog>
+        <form>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>行動記録</DialogTitle>
+              <DialogDescription>
+                記録する行動を入力してください。
+              </DialogDescription>
+            </DialogHeader>
+            <div>
+              <p>行動</p>
+              <select>
+                {types.map((type) => (
+                  <option key={type.id} value={type.id}>
+                    {type.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </DialogContent>
+        </form>
+      </Dialog>
     </>
   );
 }
