19faa115de27e8656b4e76c5803f43ccf335e83f
yskw0904 <yoshikawa.y0904@gmail.com>
2026-05-07 18:12:53 +0900

    アクティビティのフォームの機能追加

--- frontend/app/(dashboard)/activity/create/activityForm.tsx
+++ frontend/app/(dashboard)/activity/create/activityForm.tsx
@@ -18,21 +18,25 @@ type ActivityType = {
 
 export default function ActivityForm({ types }: { types: ActivityType[] }) {
   const [error, setError] = useState("");
+  const [selectedType, setSelectedType] = useState("");
+  const [selectStartTime, setSelectStartTime] = useState("");
+  const [selectEndTime, setSelectEndTime] = useState("");
+  const [note, setNote] = useState("");
 
   const submitStore = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("");
 
     const data = {
-      activity_type: "sleep",
-      start_time: "2026-04-21 10:00",
-      end_time: "2026-04-21 10:01",
-      note: "test",
+      activity_type: selectedType,
+      start_time: selectStartTime,
+      end_time: selectEndTime,
+      note: note,
     };
 
     try {
       await axios.post("/api/activity/store", data);
-      console.log("成功！");
+      console.log("成功！", data);
       // 成功時の処理（ダイアログを閉じるなど）
     } catch (error: any) {
       if (error.response?.status === 401) {
@@ -58,14 +62,41 @@ export default function ActivityForm({ types }: { types: ActivityType[] }) {
             <p className="text-red-500">{error}</p>
           </DialogHeader>
           <div className="space-y-4 flex flex-col">
-            <p>行動</p>
-            <select>
+            {/* 種別 */}
+            <select
+              className="border-2 rounded"
+              onChange={(e) => setSelectedType(e.target.value)}
+            >
               {types.map((type) => (
                 <option key={type.id} value={type.id}>
                   {type.name}
                 </option>
               ))}
             </select>
+            {/* 時刻 */}
+            <div className="space-x-3 flex-1">
+              <input
+                type="time"
+                className="border-2 rounded"
+                onChange={(e) => setSelectStartTime(e.target.value)}
+                value={selectStartTime}
+              ></input>
+              <span>〜</span>
+              <input
+                type="time"
+                className="border-2 rounded"
+                onChange={(e) => setSelectEndTime(e.target.value)}
+                value={selectEndTime}
+              ></input>
+            </div>
+            {/* コメント */}
+            <input
+              type="text"
+              className="border-2 rounded"
+              placeholder="1日の振り返りコメント"
+              onChange={(e) => setNote(e.target.value)}
+              value={note}
+            ></input>
             <button type="submit" className="cursor-pointer">
               記録する
             </button>
